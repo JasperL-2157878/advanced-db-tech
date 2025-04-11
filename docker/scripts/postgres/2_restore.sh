85d2904db842bbfe5121e3245129fec88c5e55b0
Jasper Leendertse <jasper.leendertse@student.uhasselt.be>
2025-04-11 10:33:12 +0200

    Update dump

--- docker/scripts/postgres/2_restore.sh
+++ docker/scripts/postgres/2_restore.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/postgres_localhost-2025_04_06_16_10_06-dump.sql
+pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/postgres_localhost-2025_04_11_10_19_36-dump.sql
