0a5631858f99e45c2a14a532100cec0a3700c706
Jasper Leendertse <jasper.leendertse@student.uhasselt.be>
2025-05-08 09:50:17 +0200

    Prepare public

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Advanced Database Technologies
+
+Source code will **not** compile. Use as a reference.
--- cmd/build/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-docker-compose -f docker/compose.yaml --env-file .env up -d --build
